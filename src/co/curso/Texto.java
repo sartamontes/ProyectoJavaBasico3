7d98ce40f622d71324bf62cec2d84c26b177e48d
Raúl <rsimarro@gmail.com>
2023-02-07 15:22:43 +0100

    Curso Java Básico Ejercicios tema 3 "En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
     Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
    
     String[] nombres = {"", "", "", ""}"

---
+++ src/co/curso/Texto.java
@@ -0,0 +1,22 @@
+package co.curso;
+
+public class Texto {
+
+    public static void main(String[] args) {
+
+        String[] cadenas = {"Uno", "Dos", "Tres","Cuatro"};
+
+        System.out.println(concatenaCadenas(cadenas));
+    }
+
+    // Función que recoge una array de cadenas y lo concatena
+    public static String concatenaCadenas(String[] cadenas){
+
+        String cadenaFinal = "";
+        for (String cadena : cadenas) {
+            cadenaFinal += cadena;
+
+        }
+        return  cadenaFinal;
+    }
+}
